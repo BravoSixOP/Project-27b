e996286903154bb6ecaeef46afe4360aa1ab895f
BravoSixOP <tyagisaksh2008@gmail.com>
2021-02-03 20:44:05 +0530

    Created Roof class

--- roof.js
+++ roof.js
@@ -3,8 +3,8 @@ class Roof{
         var options = {
             isStatic: true
         }
-        this.body= Bodies.rectangle(400, 180, 400, 20, options);
-        this.width=400;
+        this.body= Bodies.rectangle(400, 180, 550, 20, options);
+        this.width=550;
         this.height=20;
 
         World.add(world, this.body);
