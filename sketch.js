163e2eed8bc88a65e573de8beddcca41bc1e93ff
BravoSixOP <tyagisaksh2008@gmail.com>
2021-02-03 21:12:19 +0530

    Created Bobs

--- sketch.js
+++ sketch.js
@@ -21,6 +21,12 @@ function setup() {
 	//Create the Bodies Here.
 	roof = new Roof();
 
+	bob1 = new Bob(180, 480, 55)
+	bob2 = new Bob(290, 480, 55)
+	bob3 = new Bob(400, 480, 55)
+	bob4 = new Bob(510, 480, 55)
+	bob5 = new Bob(620, 480, 55)
+
 	Engine.run(engine);
   
 }
@@ -28,11 +34,17 @@ function setup() {
 
 function draw() {
 	rectMode(CENTER);
-	background(0);
+	background(220);
 
 	drawSprites();
 	
 	roof.display();
+
+	bob1.display();
+	bob2.display();
+	bob3.display();
+	bob4.display();
+	bob5.display();
 }
 
 
