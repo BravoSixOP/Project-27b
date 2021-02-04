25fbaef7a8e10bd59510a63bd73cd0d56a63588a
BravoSixOP <tyagisaksh2008@gmail.com>
2021-02-04 16:54:59 +0530

    Created Rope Class

--- rope.js
+++ rope.js
@@ -1,13 +1,22 @@
-class Rope {
-    constructor(bodyA, bodyB){
-        var options= {bodyA: bodyA, bodyB:bodyB, stiffness: 0.04, length: 10}
+class rope {
+    constructor(bodyA, bodyB, offsetX, offsetY){
+        this.offsetX=offsetX
+        this.offsetY=offsetY
+        var options= {
+            bodyA: bodyA,
+            bodyB:bodyB,
+            pointB:{x:this.offsetX, y:this.offsetY}
+        }
         this.chain= Constraint.create(options);
         World.add(world, this.chain);
     }
     display(){
         var pointA = this.chain.bodyA.position;
         var pointB = this.chain.bodyB.position;
+
+        var point2X = pointB.x+this.offsetX;
+        var point2Y = pointB.y+this.offsetY;
         
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        line(pointA.x, pointA.y, point2X, point2Y);
     }
 }
